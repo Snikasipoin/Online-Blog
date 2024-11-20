b5271b992c3a5c87933508fe2244540d031e0b89
Roman Kalmykow <snikasipoin@gmail.com>
2024-11-20 22:28:43 +0600

    Футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import './Footer.css';
+import { FaCaretSquareUp } from 'react-icons/fa';
 
 function Footer() {
+  function scrollTo() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    return scrollTo;
+  }
   return (
     <div className="footer">
-      <p>© Сopyright blog</p>
+      <span>
+        <p>© Copyright blog 2024</p>
+        <a href="https://github.com/Snikasipoin/Online-Blog">
+          Политика конфиденциальности
+        </a>
+      </span>
+      <button onClick={scrollTo()}>
+        ВВЕРХ
+        <FaCaretSquareUp size="36px" color="white" />
+      </button>
     </div>
   );
 }
